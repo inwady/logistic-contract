2864efc0b49463c49f16b5b26fb29eb228a15e1b
Inwady <inwady@gmail.com>
2018-02-10 17:24:20 +0300

    initial commit

---
+++ contracts/Logistic.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.17;
+
+contract Logistic {
+//    struct Place {
+//        string companyName;
+//        address companyAddress;
+//        bytes32 secret;
+//    }
+//
+//    Place[] basicPlaces;
+//    function Logistic(Place[] places) public {
+//
+//    }
+//
+//    function getMyPath() public view returns (Place[]) {
+//        return basicPlaces;
+//    }
+//
+//    function acceptSecret(string secret) {
+//
+//    }
+}
---
+++ test/Logistic.js
@@ -0,0 +1,13 @@
+
+contract("Logistic", function(accounts) {
+    const role = {
+        sender: accounts[0],
+        receiver: accounts[1],
+        trainCompany: accounts[2],
+        airCompany: accounts[3]
+    };
+
+    it("base smoke test", function() {
+
+    });
+});
